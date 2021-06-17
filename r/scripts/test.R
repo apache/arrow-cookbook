cac5a55d96f0f471d419348e655394738dd60530
Nic Crane <thisisnic@gmail.com>
2021-06-17 17:36:28 +0100

    Add Apache 2 license

--- r/scripts/test.R
+++ r/scripts/test.R
@@ -1,3 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #' Extract R code from .Rmd files
 #'
 #' Extracts all the R code and tests from .Rmd files and puts them in separate files
@@ -20,7 +36,8 @@ extract_r_code <- function(file, dir){
   knitr::purl(
     input = file,
     output = outpath,
-    # If we output text chunks as comments, the line number where the error was should match with the one reported in the tempfile; needs more testing
+    # If we output text chunks as comments, the line number where the error is
+    # reported in the tests should match with the correct line in the Rmd
     documentation = 2L,
     quiet = TRUE
   )
