616aa7093191df3dab6883c5e2172d468a3a59ef
david dali susanibar arce <davi.sarces@gmail.com>
2022-02-03 18:47:11 -0500

    Solving jni errors for jni *.dylib and *.so library dependencies

--- java/ext/javadoctest.py
+++ java/ext/javadoctest.py
@@ -36,6 +36,7 @@ class JavaDocTestBuilder(DocTestBuilder):
             cwd=path_arrow_project,
             text=True,
         )
+
         if not (path_arrow_project / ".cp.tmp").exists():
             raise RuntimeError(
                 __("invalid process to create jshell dependencies library")
@@ -48,6 +49,15 @@ class JavaDocTestBuilder(DocTestBuilder):
             raise RuntimeError(
                 __("invalid process to list jshell dependencies library")
             )
+
+        output = subprocess.getoutput("unzip /home/runner/.m2/repository/org/apache/arrow/arrow-dataset/6.0.0/arrow-dataset-6.0.0.jar")
+        print(output)
+
+        ls_output = subprocess.getoutput("ls -latr")
+        print(ls_output)
+
+        ldd_output = subprocess.getoutput("ldd libarrow_dataset_jni.so")
+        print(ldd_output)
 
         # execute java testing code thru jshell and read output
         # JDK11 support '-' This allows the pipe to work as expected without requiring a shell
