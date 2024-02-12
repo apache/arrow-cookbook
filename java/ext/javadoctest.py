e2025bcd17ac4fadd40529e35b6a89433f9e6535
david dali susanibar arce <davi.sarces@gmail.com>
2024-02-11 19:07:35 -0500

    fix: avro packages changed + rolback testing errors

--- java/ext/javadoctest.py
+++ java/ext/javadoctest.py
@@ -47,10 +47,6 @@ class JavaDocTestBuilder(DocTestBuilder):
     ) -> Any:
         # go to project that contains all your arrow maven dependencies
         path_arrow_project = pathlib.Path(__file__).parent.parent / "source" / "demo"
-        # delete classpath java dependencies files used
-        classpath_java_deps = os.path.join(path_arrow_project, '.cp.tmp')
-        if os.path.exists(classpath_java_deps):
-            os.remove(classpath_java_deps)
         # create list of all arrow jar dependencies
         subprocess.check_call(
             [
--- java/source/conf.py
+++ java/source/conf.py
@@ -38,7 +38,7 @@ copyright = '2022, Apache Software Foundation'
 author = 'The Apache Software Foundation'
 arrow_nightly=os.getenv("ARROW_NIGHTLY")
 if arrow_nightly and arrow_nightly != '0':
-    version = "15.0.0"
+    version = "16.0.0-SNAPSHOT"
 else:
     version = "15.0.0"
 print(f"Running with Arrow version: {version}")
