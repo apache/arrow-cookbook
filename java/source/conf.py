92e5aac4ef7093e1a54faa56e0d49056c245fbd8
Bryce Mecum <petridish@gmail.com>
2024-11-29 17:48:21 -0800

    MINOR: [Release] Update versions for 18.1.0 (#370)

--- java/source/conf.py
+++ java/source/conf.py
@@ -40,7 +40,7 @@ arrow_nightly=os.getenv("ARROW_NIGHTLY")
 if arrow_nightly and arrow_nightly != '0':
     version = "19.0.0-SNAPSHOT"
 else:
-    version = "18.0.0"
+    version = "18.1.0"
 print(f"Running with Arrow version: {version}")
 
 # -- General configuration ---------------------------------------------------
