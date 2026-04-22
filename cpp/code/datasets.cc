576fd9553dcf5f232d4839b541286954a7c0a29c
Bryce Mecum <petridish@gmail.com>
2026-04-22 16:58:18 -0700

    Fix deprecation

--- cpp/code/datasets.cc
+++ cpp/code/datasets.cc
@@ -87,9 +87,8 @@ class DatasetReadingTest : public ::testing::Test {
                           fs->OpenInputFile(airquality_path));
     std::unique_ptr<parquet::ParquetFileReader> parquet_reader =
         parquet::ParquetFileReader::Open(file);
-    std::unique_ptr<parquet::arrow::FileReader> reader;
-    ARROW_RETURN_NOT_OK(parquet::arrow::FileReader::Make(
-        arrow::default_memory_pool(), std::move(parquet_reader), &reader));
+    ARROW_ASSIGN_OR_RAISE(auto reader, parquet::arrow::FileReader::Make(
+        arrow::default_memory_pool(), std::move(parquet_reader)));
     std::shared_ptr<arrow::Table> table;
     ARROW_RETURN_NOT_OK(reader->ReadTable(&table));
     return table;
